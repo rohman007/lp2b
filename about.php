ee80ee8dc25b43cfe48bae4240d923ac6d2c543a
rohman007 <a.nurrohman007@gmail.com>
2023-10-19 21:26:18 +0700

    about us

--- about.php
+++ about.php
@@ -2,12 +2,21 @@
 	include "header.php";
 	include "sidebar.php";
 ?>
-	<section>
-		<div class="container">
-			<p>LP2B (Lahan Pertanian Pangan Berkelanjutan) adalah bidang lahan pertanian yang ditetapkan untuk dilindungi dan dikembangkan secara konsisten guna menghasilkan pangan pokok bagi kemandirian, ketahanan dan kedaulatan pangan nasional.</p>
-			<p>Dengan menjaga lahan pertanian ini, kita dapat memastikan pasokan pangan yang stabil dan berkelanjutan, serta mendukung keberlanjutan sumber daya alam yang berperan penting dalam ekonomi dan keberlanjutan lingkungan.</p>
-			<p>Lahan-lahan ini diprioritaskan untuk pertanian yang berkelanjutan, di mana praktik-praktik bertanggung jawab terhadap lingkungan, penggunaan air yang efisien, dan inovasi teknologi digunakan untuk mengoptimalkan hasil panen.</p>
-			<p>Dengan upaya kolaboratif melalui LP2B, kita dapat memastikan bahwa negeri ini memiliki sumber daya pertanian yang kuat, mampu memenuhi kebutuhan pangan rakyat, serta menjaga kualitas lingkungan demi masa depan yang lebih baik.</p>
+	<style>
+		body{background:url(dist/img/bg-about.jpg);background-size:cover;background-repeat:no-repeat}
+		a.burger-menu {border: 1px solid #444;}
+	</style>
+	<section class="about-section mt-5 pt-5 px-5">
+		<div class="container-fluid px-5">
+			<div class="row justify-content-end text-left">
+				<div class="col-12 col-md-6 text-white">
+					<div class="about-content">
+						<h1 class="fw-bold">Tentang LP2B</h1>
+						<p>LP2B (Lahan Pertanian Pangan Berkelanjutan) adalah bidang lahan pertanian yang ditetapkan untuk dilindungi dan dikembangkan secara konsisten guna menghasilkan pangan pokok bagi kemandirian, ketahanan dan kedaulatan pangan nasional.</p>
+						<p>Dengan menjaga lahan pertanian ini, kita dapat memastikan pasokan pangan yang stabil dan berkelanjutan, serta mendukung keberlanjutan sumber daya alam yang berperan penting dalam ekonomi dan keberlanjutan lingkungan.</p>	
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 <?php 
